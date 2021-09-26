4b48be2c6106709cd914956754307d9818fb53d2
Altynbek Abdrakmanov <altiwbek@gmail.com>
2021-09-26 18:55:18 +0300

    search panel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
 
 
   def index
-    @products = Product.includes(image_attachment: :blob).page(@page).per(@per_page)
+    @products = Product.includes(image_attachment: :blob)
+    @products = @products.where("lower(name) LIKE ?", "%" + params[:search].downcase + "%") if params[:search].present?
+    @products = @products.page(@page).per(@per_page)
 
     respond_to do |format|
       format.html
